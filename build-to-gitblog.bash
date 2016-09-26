d09062ce09d564372719edb48919c163cffca3f5
Sohan Chowdhuruy <sifat3d@gmail.com>
2016-09-26 21:06:32 +0600

    convert config file to toml. Recreate what-is-reddit post. New modified buld bash script.

--- build-to-gitblog.bash
+++ build-to-gitblog.bash
@@ -1,8 +1,33 @@
 #!/bin/bash
-rm -rf ../SohanChy.github.io/blog/*
-hugo -b="http://sohanchy.github.io/blog" -d "../SohanChy.github.io/blog/";
-cd ../SohanChy.github.io/blog/;
-git add .;
-git commit;
-git push;
 
+if [ ! -d "public" ]; then
+    echo "No public folder, exiting..."
+    exit;
+fi
+
+echo "commit and push master first?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git add .; git commit;
+        git push; 
+        break;;
+        No ) break;;
+    esac
+done
+
+mv public ../.tmp-public
+git checkout -f gh-pages
+rm -rf ./* 
+mv ../.tmp-public/* ./
+rmdir ../.tmp-public
+
+git add .
+git commit
+
+echo "Wanna push?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git push; git checkout master; break;;
+        No ) exit;;
+    esac
+done
